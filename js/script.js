9733773cbf62db4a434d5246928f3dbd54d07072
Maxim-g1 <122662234+Maxim-g1@users.noreply.github.com>
2024-05-11 14:51:18 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,62 @@
+let currentPlayer = "X"
+const btn = document.querySelector('.btn')
+let gameEnded = false
+
+let board = ["", "", "", "", "", "", "", "", "",]
+
+const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6],
+];
+
+function cellClicked(cellIndex) {
+    if (!gameEnded && board[cellIndex] === "") {
+        const cell = document.getElementById(`cell${cellIndex}`)
+        cell.textContent = currentPlayer
+        cell.setAttribute('data-value', currentPlayer)
+        board[cellIndex] = currentPlayer
+        if (checkWinner(currentPlayer)) {
+            document.getElementById("message").textContent = `Игрок ${currentPlayer} победил!`
+            gameEnded = true
+            btn.style.display = 'block'
+        } else if (isBoardFull()) {
+            document.getElementById("message").textContent = "Ничья!"
+            gameEnded = true
+            btn.style.display = 'block'
+        } else {
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
+        }
+    }
+}
+
+if (currentPlayer === "O") {
+    for (bo of board) {
+        if (!gameEnded && bo === "") {
+            let randomIndex = Math.random(bo)
+            const cell = document.getElementById(`cell${randomIndex}`)
+
+            cell.textContent = currentPlayer
+            cell.setAttribute('data-value', currentPlayer)
+            bo[randomIndex] = currentPlayer
+
+
+        }
+    }
+}
+
+
+
+function checkWinner(player) {
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern
+        if (board[a] === player && board[b] === player && board[c] === player) {
+            return true
+        }
+    }
+    return false
+}
+
+function isBoardFull() {
+    return board.every(cell => cell !== "")
+}
